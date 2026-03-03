aea4488949e80fed4cb8e5ec9f2c300e250b9e93
daniilraptanov <daniilraptanov@gmail.com>
2026-03-03 18:32:07 +0200

    Initial commit: Darkroom PWA timer

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "darkroom-timer-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
